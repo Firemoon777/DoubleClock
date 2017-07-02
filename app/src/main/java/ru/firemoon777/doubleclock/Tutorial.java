cc2b3e9e6208dad3081917f68a0f7066101ea303
root <root@chimera>
2017-07-02 18:06:22 +0300

    init

---
+++ app/src/main/java/ru/firemoon777/doubleclock/Tutorial.java
@@ -0,0 +1,44 @@
+package ru.firemoon777.doubleclock;
+
+import android.graphics.Color;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class Tutorial implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.android.systemui"))
+            return;
+
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm");
+                try {
+                    Calendar calendar = Calendar.getInstance();
+                    Date date = dateFormat.parse(text);
+                    calendar.setTime(date);
+                    calendar.add(Calendar.HOUR, -2);
+                    text = text + " / " + dateFormat.format(calendar.getTime());
+                } catch (Exception e) {
+                    tv.setTextColor(Color.RED);
+                }
+                tv.setText(text);
+            }
+        });
+    }
+}
